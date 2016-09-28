a04268dd38d524ecfd588c70202df1c667ae8f39
Yaodoggy <602975805@qq.com>
2016-09-28 18:44:28 +0800

    fix: 逻辑拆分;
    Signed-off-by: Yaodoggy <602975805@qq.com>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
+// 基本规则坚持
+var commanRule = function () {
+    var html = '';
+    var cssName = '';
+    (function () {
+        fs.readdirSync(cwd).forEach(function (item) {
+            if (item.match(/\.html$/)) htmlName = cwd + '/' + item;
+        });
+    }());
+    if (!htmlName) {
+        console.log('当前目录下没有对应的html文件!');
+        return;
+    }
+};
+
 var helper = function (params) {
     var html = require('./html-helper');
     html.init();
 };
 
+var jade = function (params) {
+};
+
 
 module.exports = {
-    init: helper
+    init: helper,
+    jade: jade
 };
